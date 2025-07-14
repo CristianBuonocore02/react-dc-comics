0ffcb588f376376ac31738c390ef1a104d152e08
Cristian Buonocore <cristian.buonocore680@gmail.com>
2025-07-15 00:37:33 +0200

    update Main component layout and styles; add image links for digital comics and merchandise

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,30 @@
 export default function Main() {
     return (
-        <main>
-            <h1>Prova Main</h1>
+        <main className="flexMain">
+            <div className="backgroundMain">
+                <div className="listMain">
+                    <div>
+                        <img src="public/buy-comics-digital-comics.png" alt="" />
+                        <a href="">digital comics</a>
+                    </div>
+                    <div>
+                        <img src="public/buy-comics-merchandise.png" alt="" />
+                        <a href="">dc merchandise</a>
+                    </div>
+                    <div>
+                        <img src="public/buy-comics-subscriptions.png" alt="" />
+                        <a href="">subscriptions</a>
+                    </div>
+                    <div>
+                        <img src="public/buy-comics-shop-locator.png" alt="" />
+                        <a href="">comic shop locator</a>
+                    </div>
+                    <div>
+                        <img src="public/buy-dc-power-visa.svg" alt="" />
+                        <a href="">dc power visa</a>
+                    </div>
+                </div>
+            </div>
         </main>
     )
 }
